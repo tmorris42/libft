318b0cb0f3ef8a8df0d054c93e06d33482158109
tmorris <tmorris@student.42.fr>
2020-11-16 14:31:46 +0100

    Add ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 14:04:38 by tmorris           #+#    #+#             */
+/*   Updated: 2020/11/16 14:19:54 by tmorris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memccpy(void *dest, const void *src, int c, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	unsigned char		stop;
+
+	d = (unsigned char*)dest;
+	s = (const unsigned char*)src;
+	stop = (unsigned char)c;
+	if (!d && !s)
+		return (dest);
+	while (n-- > 0)
+	{
+		*d++ = *s++;
+		if (*(d - 1) == stop)
+			return (d);
+	}
+	return (NULL);
+}

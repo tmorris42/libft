956381a2266fff649fa2e43e4a3e83840b6619de
tmorris <tmorris@student.42.fr>
2020-11-17 15:02:19 +0100

    Update ft_strlcat ft_strlcpy to pass tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 20:20:41 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/16 20:29:33 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/17 14:26:32 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	dst_len;
+	size_t	src_len;
 
 	i = 0;
-	dst_len = ft_strlen(dst);
-	if (size <= 0)
-		return (dst_len + ft_strlen(src));
-	if (size < dst_len)
-	{
-		dst[size - 1] = '\0';
-		return (dst_len + ft_strlen(src));
-	}
+	dst_len = 0;
+	src_len = ft_strlen(src);
+	while (dst_len < size && dst[dst_len])
+		dst_len++;
+	if (!src)
+		return (dst_len);
+	if (dst_len == size)
+		return (size + ft_strlen(src));
 	while (src[i] && dst_len + i < size - 1)
 	{
 		dst[dst_len + i] = src[i];
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 20:12:35 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/16 20:32:48 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/17 15:00:49 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t src_len;
 
 	i = 0;
+	if (!src)
+		return (0);
+	src_len = ft_strlen(src);
 	if (size <= 0)
-		return (ft_strlen(src));
+		return (src_len);
 	while (src[i] && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (src_len);
 }

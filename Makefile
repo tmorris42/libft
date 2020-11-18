1b7167f07c03158a048294c9b97d274d5f0f2faa
tmorris <tmorris@student.42.fr>
2020-11-18 18:43:26 +0100

    Add ft_putchar_fd, ft_putstr_fd, and ft_putendl_fd

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/16 13:33:17 by tmorris           #+#    #+#              #
-#    Updated: 2020/11/18 17:38:13 by tmorris          ###   ########.fr        #
+#    Updated: 2020/11/18 18:33:59 by tmorris          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,8 @@ SRCS = ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c \
 	   ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
 	   ft_isprint.c ft_toupper.c ft_tolower.c \
 	   ft_calloc.c ft_strdup.c ft_strmapi.c ft_strjoin.c ft_strtrim.c \
-	   ft_split.c ft_itoa.c ft_substr.c
+	   ft_split.c ft_itoa.c ft_substr.c \
+	   ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c
 
 OBJS = ${SRCS:.c=.o}
 
--- libft.h
+++ libft.h
@@ -6,13 +6,14 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 13:30:18 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/18 17:37:51 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/18 18:31:51 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -48,7 +49,9 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif

fb06b3c53b13048479ea0cc748440d459ff3e47a
tmorris <tmorris@student.42.fr>
2020-11-18 17:15:56 +0100

    Finish ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,27 +6,29 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 13:48:10 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/18 14:30:55 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/18 17:15:06 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_strings(char const *s, char c)
+static int	ft_count_strs(char const *s, char c)
 {
 	int		count;
-	
+
 	count = 0;
 	while (*s)
 	{
-		while (ft_memcmp(s, &c, 1))
-		   s++;
-		if (*s != '\0')
-		{	
+		while (*s == c)
+			s++;
+		if (*s)
+		{
 			count++;
-			while (*s && !ft_memcmp(s, &c, 1))
+			while (*s && *s != c)
 				s++;
 		}
+		else
+			s++;
 	}
 	return (count);
 }
@@ -36,7 +38,7 @@ static int	ft_segment_len(char const *s, char c)
 	int		len;
 
 	len = 0;
-	while (*s && !ft_memcmp(s, &c, 1))
+	while (*s && *s != c)
 	{
 		s++;
 		len++;
@@ -54,29 +56,29 @@ static void	ft_free_strings(char **strings)
 		free(strings[i]);
 		i++;
 	}
+	free(strings);
 }
 
 char		**ft_split(char const *s, char c)
 {
-	int		array_len;
 	char	**array;
 	int		i;
 
 	i = 0;
-	array_len = ft_count_strings(s, c);
-	if (!(array = (char **)ft_calloc(array_len + 1, sizeof(char*))))
+	if (!s)
+		return (NULL);
+	if (!(array = (char **)ft_calloc(ft_count_strs(s, c) + 1, sizeof(char*))))
 		return (NULL);
 	while (*s)
 	{
-		while (s && ft_memcmp(s, &c, 1))
+		while (*s && *s == c)
 			s++;
 		if (*s == '\0')
 			break ;
-		if (!(array[i] == ft_substr(s, 0, ft_segment_len(s, c))))
+		if (!(array[i] = ft_substr(s, 0, ft_segment_len(s, c))))
 		{
 			ft_free_strings(array);
-			free(array);
-			return(NULL);
+			return (NULL);
 		}
 		i++;
 		s += ft_segment_len(s, c);

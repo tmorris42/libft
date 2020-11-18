0da9df03c80a202c7691f1ad5ef7352b30426e57
tmorris <tmorris@student.42.fr>
2020-11-18 18:21:30 +0100

    Fix ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 17:36:49 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/18 18:09:10 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/18 18:20:56 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,11 @@ static void	ft_strrev(char *str)
 	}
 }
 
-char		*ft_itoa(int n)
+static char	*ft_gen_str(char *str, int n, int sign)
 {
-	int		sign;
-	int		len;
-	char	*str;
 	int		i;
 
-	sign = 1;
-	if (n < 0)
-		sign = -1;
-	len = 0;
-	i = n;
-	while (i != 0)
-	{
-		i = i / 10;
-		len++;
-	}
-	if (!(str = (char *)malloc(sizeof(*str) * (len + 1 + (sign == -1)))))
-		return (NULL);
+	i = 0;
 	if (n == 0)
 	{
 		str[0] = '0';
@@ -62,6 +48,29 @@ char		*ft_itoa(int n)
 	if (sign == -1)
 		str[i++] = '-';
 	str[i] = '\0';
+	return (str);
+}
+
+char		*ft_itoa(int n)
+{
+	int		sign;
+	int		len;
+	char	*str;
+	int		i;
+
+	sign = 1;
+	if (n < 0)
+		sign = -1;
+	len = 1;
+	i = n;
+	while (i != 0)
+	{
+		i = i / 10;
+		len++;
+	}
+	if (!(str = (char *)malloc(sizeof(*str) * (len + (sign == -1) + (n == 0)))))
+		return (NULL);
+	str = ft_gen_str(str, n, sign);
 	ft_strrev(str);
 	return (str);
 }

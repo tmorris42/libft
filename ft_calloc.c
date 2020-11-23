d2c556193c21fe2d1fbb8a245be3d39f35884356
tmorris <tmorris@student.42.fr>
2020-11-23 16:09:58 +0100

    Simplify ft_calloc.c to use bzero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 18:52:15 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/17 18:57:53 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/23 15:15:55 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,9 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	char	*space;
-	size_t	i;
 
 	if (!(space = (char *)malloc(count * size)))
 		return (NULL);
-	i = 0;
-	while (i < count * size)
-	{
-		space[i] = 0;
-		i++;
-	}
+	ft_bzero(space, count * size);
 	return ((void *)space);
 }

1b5966d4557cba66e8576048a18ae5204bc32a9f
tmorris <tmorris@student.42.fr>
2020-11-25 16:26:46 +0100

    Update ft_strlcat.c to use src_len instead of repeat calls to strlen

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 20:20:41 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/19 16:42:45 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/11/25 15:26:52 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	if (!src)
 		return (dst_len);
 	if (dst_len == dstsize)
-		return (dstsize + ft_strlen(src));
+		return (dstsize + src_len);
 	while (src[i] && dst_len + i < dstsize - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (dst_len + ft_strlen(src));
+	return (dst_len + src_len);
 }

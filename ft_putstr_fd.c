8f22a17ee07424692fc8e58e61f577a001e899bc
tmorris <tmorris@student.42.fr>
2021-10-05 10:38:54 +0200

    Make ft_putstr_fd accept only const char *s
    This allows the passing of const strings directly to ft_putstr_fd without casting

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 18:30:14 by tmorris           #+#    #+#             */
-/*   Updated: 2020/11/23 19:01:19 by tmorris          ###   ########.fr       */
+/*   Updated: 2021/09/20 12:18:20 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(const char *s, int fd)
 {
 	if (s)
 		write(fd, s, ft_strlen(s));

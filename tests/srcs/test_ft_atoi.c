053d2ef2877e9690705405099351b70803ae69c4
tmorris <tmorris@student.42.fr>
2022-03-26 14:58:57 +0100

    Update test_ft_atoi.c
    Compare with real atoi

--- tests/srcs/test_ft_atoi.c
+++ tests/srcs/test_ft_atoi.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "libft.h"
 
 static int comp(int a, int b)
@@ -23,26 +24,22 @@ int main()
 		"-2147483660",
 		"2147483650",
 		"2147483648",
-		0};
-	int expected[] = {
-		-2147483648,
-		-5,
-		5,
-		2147483647, 
-		0,
-		-58,
-		2147483647,
-		2147483646,
-		2147483636,
-		-2147483646,
-		-2147483648,
+		"42",
+		"674832",
+		"--3161631",
+		"+426161",
+		"++2626",
+		"+et4",
+		"+-+-+-+--62727",
 		0};
 	int i;
 
 	i = 0;
 	while (1)
 	{
-		if (!comp(ft_atoi(values[i]), expected[i]))
+		// if (!comp(ft_atoi(values[i]), expected[i]))
+		// 	return (1);
+		if (!comp(ft_atoi(values[i]), atoi(values[i])))
 			return (1);
 		++i;
 		if (values[i] == 0)

8af82a88b9b77514e5c9ec29b6bc64f95c9cf9bd
tmorris <tmorris@student.42.fr>
2022-10-28 14:08:50 +0200

    test: create test_ft_isalpha.c

---
+++ tests/srcs/test_ft_isalpha.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_isalpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/24 13:54:28 by tmorris           #+#    #+#             */
+/*   Updated: 2021/06/21 15:13:31 by tmorris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include "libft.h"
+
+static int	comp(int a, int b)
+{
+	ft_putnbr(a);
+	ft_putstr(" == ");
+	ft_putnbr(b);
+	ft_putchar('\n');
+	return ((a > 0) == (b > 0));
+}
+
+int	main(void)
+{
+	int				i;
+
+	i = 0;
+	while (1)
+	{
+		if (!comp(ft_isalnum(i), isalnum(i)))
+			return (1);
+		++i;
+		if (i == 256)
+			break ;
+	}
+	return (0);
+}
